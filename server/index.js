d9e55d70cfa1194d64c8786ee07121f15df869be
kalon1997 <kalonacharjee@gmail.com>
2022-07-22 14:32:12 +0530

    path colon2

--- server/index.js
+++ server/index.js
@@ -53,7 +53,7 @@ io.on("connection",async (socket)=>{
 if (process.env.NODE_ENV === "production") {
   server.use(express.static({path: __dirname + "../client/build"}));
     server.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+      res.sendFile({path: __dirname + "../client/build/index.html"});
     });
   }
 
